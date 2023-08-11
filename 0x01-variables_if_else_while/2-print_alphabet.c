bd908825e54482165dbcc0efb43c0b3c894f7332
fadwa023 <fadooshbakri@hotmail.com>
2023-08-11 21:08:30 +0000

    C variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+char f = 'a';
+while (f <= 'z')
+
+putchar(f);
+f++;
+
+putchar('\n');
+return (0);
+}

5b7e057c1f3b2f1d2d76c43be41460d199b4d5e9
fadwa023 <fadooshbakri@hotmail.com>
2023-08-16 23:43:55 +0000

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,38 +2,22 @@
 
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-int h1 = 0;
-int h2 = 0;
-int m1 = 0;
-int m2 = 0;
-while (1)
+int hour, minute;
+
+for (hour = 0; hour < 24; hour++)
 {
-putchar(h1 + '0');
-putchar(h2 + '0');
+for (minute = 0; minute < 60; minute++)
+{
+putchar('0' + hour / 10);
+putchar('0' + hour % 10);
 putchar(':');
-putchar(m1 + '0');
-putchar(m2 + '0');
+putchar('0' + minute / 10);
+putchar('0' + minute % 10);
 putchar('\n');
-m2++;
-if (m2 == 10)
-{
-m2 = 0;
-m1++;
 }
-if (m1 == 6)
-{
-m1 = 0;
-h2++;
-}
-if (h2 == 9)
-{
-h2 = 0;
-h1++;
-}
-if (h1 == 2 && h2 == 4 && m1 == 0 && m2 == 0)
-break;
 }
 }

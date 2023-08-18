fb485af3d9417606e2ff60982c78120a20ee3715
fadwa023 <fadooshbakri@hotmail.com>
2023-08-18 16:43:02 +0000

    functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+ #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -13,6 +13,8 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
 int _putchar(char c);
 
 #endif /* MAIN_H */
